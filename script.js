f33ff82c630989144fd9d914018363fbe0c46a16
Shashank Rajput <hariomshashank@gmail.com>
2023-11-04 13:31:29 +0530

    fixed the bug of pawn (box - 31 to box - 38 )

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let main = document.querySelector(".main");
 
 let count = 1; // used to define colours 
 
+
 for(let x = 0; x < 64; x++){
     let div = document.createElement("div");
     div.className = `div-${x}`;
@@ -21,7 +22,6 @@ for(let x = 0; x < 64; x++){
 
     if(x == 0 || x == 7 ){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/blacks/elephant.png";
         img.src= "elephantb.png";
         img.classList.add(`elephant`);
         img.classList.add(`black`);
@@ -31,7 +31,6 @@ for(let x = 0; x < 64; x++){
 
     if(x == 4){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/blacks/king.png";
         img.src= "kingb.png";
         img.classList.add(`king`);
         img.classList.add(`black`);
@@ -40,7 +39,6 @@ for(let x = 0; x < 64; x++){
     }
     if(x >= 8 && x < 16){ // used to set pawns on their place 
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/blacks/pawn-black.png";
         img.src= "pawn-black.png";
         img.classList.add(`pawns`);
         img.classList.add(`black`);
@@ -51,7 +49,6 @@ for(let x = 0; x < 64; x++){
 
     if(x == 2 || x == 5){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/blacks/camel.png";
         img.src= "camelb.png";
         img.classList.add(`camel`);
         img.classList.add(`black`);
@@ -60,7 +57,6 @@ for(let x = 0; x < 64; x++){
     }
     if(x == 1 || x == 6){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/blacks/horse.png";
         img.src= "horseb.png";
         img.classList.add(`horse`);
         img.classList.add(`black`);
@@ -69,7 +65,6 @@ for(let x = 0; x < 64; x++){
     }
     if(x == 3){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/blacks/minister.png";
         img.src= "ministerb.png";
         img.classList.add("minister");
         img.classList.add(`black`);
@@ -81,7 +76,6 @@ for(let x = 0; x < 64; x++){
 
     if(x >= 48 && x <= 55){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/whites/pawn.png";
         img.src= "pawnw.png";
         img.classList.add("pawns-2");
         img.classList.add(`white`);
@@ -91,7 +85,6 @@ for(let x = 0; x < 64; x++){
 
     if(x == 63 || x == 56){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/whites/elephant.png";
         img.src= "elephantw.png";
         img.classList.add("elephant");
         img.classList.add(`white`);
@@ -100,7 +93,6 @@ for(let x = 0; x < 64; x++){
     }
     if(x == 62 || x == 57){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/whites/horse.png";
         img.src= "horsew.png";
         img.classList.add("horse");
         img.classList.add(`white`);
@@ -110,7 +102,6 @@ for(let x = 0; x < 64; x++){
 
     if(x == 61 || x == 58){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/whites/camel.png";
         img.src= "camelw.png";
         img.classList.add("camel");
         img.classList.add(`white`);
@@ -120,7 +111,6 @@ for(let x = 0; x < 64; x++){
 
     if(x == 60){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/whites/minister.png";
         img.src= "ministerw.png";
         img.classList.add("minister");
         img.classList.add(`white`);
@@ -129,7 +119,6 @@ for(let x = 0; x < 64; x++){
     }
     if(x == 59){
         let img = document.createElement("img");
-        // img.src= "https://raw.githubusercontent.com/rajputshashank003/Chess-0.01/main/images/whites/king.png";
         img.src= "kingw.png";
         img.classList.add("king");
         img.classList.add(`white`);
@@ -139,6 +128,8 @@ for(let x = 0; x < 64; x++){
 
     main.appendChild(div);
 }
+
+let moveCount = 0;
 
 let boxes = document.querySelectorAll(".inner-div");
 let alreadyselected ; // it is an index for the div[alreadyselected] ;
@@ -165,7 +156,7 @@ function forPawnsBlacks(index){
                 boxes[index].removeChild(boxes[index].firstElementChild);
                 boxes[index].appendChild(alreadyselected);
                 // console.log("removed");
-                boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+                boxes[index].childNodes[0].classList.remove("img-extrashow");
                 asInd = undefined;
                 alreadyselected = undefined;
                 
@@ -191,7 +182,6 @@ function forPawnsWhites(index){
         if(asInd >= 48 && asInd <= 55 && index == asInd -8 - 8 && boxes[index].childNodes.length == 0 ){
             boxes[index].appendChild(alreadyselected);
             boxes[index].childNodes[0].classList.remove("img-extrashow");
-            console.log("moved");
             asInd = undefined;
             alreadyselected = undefined;
         } else if(index == asInd - 8 && boxes[index].childNodes.length == 0){
@@ -225,24 +215,70 @@ function forPawnsWhites(index){
 }
 
 function moveFunc1(index){
-    if(boxes[index].childNodes.length == 0){
+    if(boxes[asInd].childNodes[0].classList.contains("black") && moveCount == 0){
+        if(boxes[index].childNodes.length == 0){
+            boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+            // boxes[index].childNodes[0].classList.remove("img-extrashow");
+            boxes[index].appendChild(alreadyselected);
+        } else if(boxes[index].childNodes.length != 0 && boxes[index].childNodes[0].classList[1] != boxes[asInd].childNodes[0].classList[1] && boxes[asInd].childNodes[0].classList.contains("black")){
+            boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+            boxes[index].childNodes[0].classList.remove("img-extrashow");
+            if(boxes[index].firstChild.classList.contains("king")){
+                if(boxes[index].firstChild.classList.contains("black")){
+                    window.alert("white wins");
+                } else {
+                    window.alert("black wins");
+                }
+            }
+            boxes[index].removeChild(boxes[index].firstElementChild);
+            boxes[index].appendChild(alreadyselected);
+
+            // console.log("removed");
+        } else if(boxes[index].childNodes.length != 0 &&boxes[index].childNodes[0].classList[1] == boxes[asInd].childNodes[0].classList[1]
+            && boxes[asInd].childNodes[0].classList.contains("black")){
+            boxes[index].childNodes[0].classList.remove("img-extrashow");
+            boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+            alreadyselected = undefined;
+            asInd = undefined;
+            
+            // console.log("reselect anything ");
+        }
+
+        moveCount = 1;
+    } else if(boxes[asInd].childNodes[0].classList.contains("white") && moveCount == 1){
+        if(boxes[index].childNodes.length == 0){
+            boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+            // boxes[index].childNodes[0].classList.remove("img-extrashow");
+            boxes[index].appendChild(alreadyselected);
+        } else if(boxes[index].childNodes.length != 0 && boxes[index].childNodes[0].classList[1] != boxes[asInd].childNodes[0].classList[1] && boxes[asInd].childNodes[0].classList.contains("black")){
+            boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+            boxes[index].childNodes[0].classList.remove("img-extrashow");
+            if(boxes[index].firstChild.classList.contains("king")){
+                if(boxes[index].firstChild.classList.contains("black")){
+                    window.alert("white wins");
+                } else {
+                    window.alert("black wins");
+                }
+            }
+            boxes[index].removeChild(boxes[index].firstElementChild);
+            boxes[index].appendChild(alreadyselected);
+
+            // console.log("removed");
+        } else if(boxes[index].childNodes.length != 0 &&boxes[index].childNodes[0].classList[1] == boxes[asInd].childNodes[0].classList[1]
+            && boxes[asInd].childNodes[0].classList.contains("black")){
+            boxes[index].childNodes[0].classList.remove("img-extrashow");
+            boxes[asInd].childNodes[0].classList.remove("img-extrashow");
+            alreadyselected = undefined;
+            asInd = undefined;
+            
+            // console.log("reselect anything ");
+        }
+
+        moveCount = 0;
+    } else {
         boxes[asInd].childNodes[0].classList.remove("img-extrashow");
-        // boxes[index].childNodes[0].classList.remove("img-extrashow");
-        boxes[index].appendChild(alreadyselected);
-    } else if(boxes[index].childNodes.length != 0 && boxes[index].childNodes[0].classList[1] != boxes[asInd].childNodes[0].classList[1]){
-        boxes[asInd].childNodes[0].classList.remove("img-extrashow");
-        boxes[index].childNodes[0].classList.remove("img-extrashow");
-        boxes[index].removeChild(boxes[index].firstElementChild);
-        boxes[index].appendChild(alreadyselected);
-        // console.log("removed");
-    } else if(boxes[index].childNodes.length != 0 &&boxes[index].childNodes[0].classList[1] == boxes[asInd].childNodes[0].classList[1]){
-        boxes[index].childNodes[0].classList.remove("img-extrashow");
-        boxes[asInd].childNodes[0].classList.remove("img-extrashow");
-        alreadyselected = undefined;
-        asInd = undefined;
-        
-        // console.log("reselect anything ");
     }
+        
 }
 
 function forCommmonElephant(index){
@@ -312,7 +348,7 @@ function forCommmonElephant(index){
             }  else if(boxes[len] && boxes[len].childNodes.length == 1){
                 // console.log(boxes[len]);
                 boxes[asInd].childNodes[0].classList.remove("img-extrashow");
-                boxes[index].childNodes[0].classList.remove("img-extrashow"); 
+                // boxes[index].childNodes[0].classList.remove("img-extrashow"); 
                 alreadyselected = undefined;
                 asInd = undefined;
                 return false;
@@ -450,7 +486,7 @@ function forKing(index){
             moveFunc1(index);
             alreadyselected = undefined;
             asInd = undefined;
-        } 
+        }
     }
 }
 
@@ -522,3 +558,10 @@ function addEventListenersForBoxes(index) {
 for (let i = 0; i < 64; i++) {
     addEventListenersForBoxes(i);
 }
+
+
+let restart = document.querySelector(".restart");
+restart.addEventListener("click", () => {
+    location.reload();
+});
+
